542e4c2865aa1cdd60e5ec042f026a14ab6f98ff
Mark Mullan <markrmullan@gmail.com>
2016-09-29 12:20:27 -0700

    prepopulate sign up form with info from clover api

--- helloworld.py
+++ helloworld.py
@@ -7,6 +7,9 @@ import webapp2
 import pdb
 import logging
 import urllib2
+from urlparse import urlparse
+# from urllib import urlparse
+import urlparse
 import json
 
 from webapp2 import WSGIApplication, Route
@@ -49,17 +52,30 @@ class MainPage(webapp2.RequestHandler):
             request = urllib2.Request("https://api.clover.com/v3/merchants/ZXWVDF5S051T2/", None, {"Authorization": "Bearer cce6bda8-4844-c126-b956-b0ceedd63519"})
             rest_api_response = urllib2.urlopen(request)
             rest_api_html = rest_api_response.read()
+            rest_api_json = json.loads(rest_api_html)
             rest_api_response.close()
+
+            address_request = urllib2.Request(rest_api_json[u'address']['href'], None, {"Authorization": "Bearer cce6bda8-4844-c126-b956-b0ceedd63519"})
+            address_file = urllib2.urlopen(address_request)
+            address = address_file.read()
+            address_file.close()
+
+            email_request = urllib2.Request(rest_api_json[u'owner']['href'], None, {"Authorization": "Bearer cce6bda8-4844-c126-b956-b0ceedd63519"})
+            email_file = urllib2.urlopen(email_request)
+            email = email_file.read()
+            email_file.close()
+
             ############################################################
             ### OBJECT TO USE FOR PARSING, AUTOCOMPLETING FORMS, ETC ###
             ############################################################
-
-            print "TEST REST API STRING IS", rest_api_html
-
-            self.redirect('http://localhost:8080/callback')
+            self.redirect('http://localhost:8080/users/new?' + urllib.urlencode({'data': {
+                                                                                          'address': address,
+                                                                                          'email': email
+                                                                                          }
+                                                                                  }))
         else:
             # No code, redirect to Clover OAuth
-            self.redirect('http://www.clover.com/oauth/authorize?client_id=E0SVKZCX95KXE&redirect_uri=http://localhost:8080/callback')
+            self.redirect('http://www.clover.com/oauth/authorize?client_id=E0SVKZCX95KXE&redirect_uri=http://localhost:8080')
 
 class Guestbook(webapp2.RequestHandler):
     def post(self):
@@ -98,11 +114,48 @@ class Callback(webapp2.RequestHandler):
         path = os.path.join(os.path.dirname(__file__), 'index.html')
         self.response.out.write(template.render(path, template_values))
 
+class CreateUser(webapp2.RequestHandler):
+    def get(self):
+
+        # data = self.request.get('data').replace("'", '"')
+        data = self.request.get('data').replace('false', "False")
+        data = eval(data)
+
+        address = eval(data['address'])
+        email = eval(data['email'])
+        # email = eval(data.get('email'))
+        print "data object is:", data
+        # print "email object is:", email
+
+        # print "DATA OBJECT RETURNED IS:", data
+        # data = json.loads(data)
+        # print data[0]
+        # data = json.loads(data.replace("'", '"'))
+
+        # print data[0]
+        # print data.address
+
+
+
+        if data:
+            # json_data = json.loads(data)
+
+
+            template_values = {
+                "address1": address.get('address1'),
+                "address2": address.get("address2"),
+                "email": email.get("email")
+            }
+
+            path = os.path.join(os.path.dirname(__file__), 'sign_up.html')
+            self.response.out.write(template.render(path, template_values))
+
 # ROUTES
 routes = [
     Route (r'/', handler = MainPage),
     Route (r'/sign', handler = Guestbook),
-    Route (r'/callback', handler = Callback)
+    Route (r'/callback', handler = Callback),
+    Route (r'/users/new', handler = CreateUser)
 ]
 
 app = webapp2.WSGIApplication(routes, debug=True)
