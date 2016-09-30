410e8e1be3d3ddf740c7c5ce8f92556ffe06ec5f
Mark Mullan <markrmullan@gmail.com>
2016-09-30 11:53:03 -0700

    rewrite http requests using GAE urlfetch library

--- helloworld.py
+++ helloworld.py
@@ -7,20 +7,21 @@ import webapp2
 import pdb
 import logging
 import urllib2
-from urlparse import urlparse
 import time
-import urlparse
 import json
+import urlparse
 
 from webapp2 import WSGIApplication, Route
 
 from google.appengine.api import users
 from google.appengine.ext import db
 from google.appengine.ext.webapp import template
+from google.appengine.api import urlfetch
 
 from secret_settings import *
 
 access_token_str = ''
+global_code = ''
 # merchant_id = 'ZXWVDF5S051T2'
 # app_id = 'E0SVKZCX95KXE' also aliased as cl\ient_id that is a query param to
 # API TOKEN cce6bda8-4844-c126-b956-b0ceedd63519
@@ -41,12 +42,18 @@ class MainPage(webapp2.RequestHandler):
     def get(self):
         code = self.request.get('code')
         if code:
+            global_code = code
             global access_token_str
 
-            response = urllib2.urlopen("https://www.clover.com/oauth/token?client_id=E0SVKZCX95KXE&client_secret=" + CLIENT_SECRET + "&code=" + code)
-            html = response.read()
-            access_token_str = str(json.loads(html)[u'access_token'])
-            response.close()
+            url = "https://www.clover.com/oauth/token?client_id=E0SVKZCX95KXE&client_secret=" + CLIENT_SECRET + "&code=" + code
+            try:
+                result = urlfetch.fetch(url)
+                if result.status_code == 200:
+                    access_token_str = str(json.loads(result.content)[u'access_token'])
+                else:
+                    self.response.status_code = result.status_code
+            except:
+                logging.exception('Caught exception fetching url')
 
             request = urllib2.Request("https://api.clover.com/v3/merchants/ZXWVDF5S051T2/", None, {"Authorization": "Bearer " + access_token_str})
             rest_api_response = urllib2.urlopen(request)
@@ -138,6 +145,8 @@ class NewUserForm(webapp2.RequestHandler):
 
 class NewOrderForm(webapp2.RequestHandler):
     def get(self):
+        template_values = {}
+
         path = os.path.join(os.path.dirname(__file__), 'new_order.html')
         self.response.out.write(template.render(path, template_values))
 
@@ -145,9 +154,42 @@ class CreateUser(webapp2.RequestHandler):
     def post(self):
         # simulate loading screen, in live app, would actually create a User
         # and store in db.
+
         print "Creating user in db, loading..."
         time.sleep(2)
         self.redirect("http://localhost:8080/orders/new")
+
+class CreateOrder(webapp2.RequestHandler):
+    def post(self):
+        params = urlparse.parse_qs(self.request.body)
+        global access_token_str
+        global global_code
+
+        # response = urllib2.urlopen("https://www.clover.com/oauth/token?client_id=E0SVKZCX95KXE&client_secret=" + CLIENT_SECRET + "&code=" + global_code)
+        # html = response.read()
+        # access_token_str = str(json.loads(html)[u'access_token'])
+        # response.close()
+
+        print access_token_str
+        print access_token_str
+        print access_token_str
+        print access_token_str
+        print access_token_str
+        print access_token_str
+
+
+        # order_request = urllib2.Request('https://www.clover.com/v3/merchants/ZXWVDF5S051T2/orders',
+        #                                 json.loads(json.dumps({
+        #                                     "note": params["note"][0],
+        #                                     "total": params["total"][0],
+        #                                     "client_id": "E0SVKZCX95KXE",
+        #                                     "client_secret": CLIENT_SECRET,
+        #                                     "code": global_code
+        #                                 })),
+        #                                 {"Authorization": "Bearer " + access_token_str}
+        #                                 )
+        # order_response = urllib2.urlopen(order_request)
+
 
 # ROUTES
 routes = [
@@ -156,7 +198,8 @@ routes = [
     Route (r'/guestbook/index', handler = GuestbookIndex),
     Route (r'/users/new', handler = NewUserForm),
     Route (r'/users/create', handler = CreateUser),
-    Route (r'/orders/new', handler = NewOrderForm)
+    Route (r'/orders/new', handler = NewOrderForm),
+    Route (r'/orders/create', handler = CreateOrder)
 ]
 
 app = webapp2.WSGIApplication(routes, debug=True)
