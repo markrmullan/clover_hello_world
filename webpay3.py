1215507c6f033cc7030d2148e63627a22bdacba2
Mark Mullan <markrmullan@gmail.com>
2017-01-11 14:08:26 -0800

    remove unnecessary config, add additional comments;

--- webpay3.py
+++ webpay3.py
@@ -5,15 +5,14 @@ import requests
 from Crypto.PublicKey import RSA
 from base64 import b64encode
 import json
-
+###############################################
 ########## BEGIN SCRIPT CONFIG SETUP ##########
+###############################################
+
 merchantID = "CNKMYYVYGJHXJ" # sandbox Test Merchant
-# merchantID = "B4DKP7TRRQHA0" # EU Prod test merchant
-target_env = "https://sandbox.dev.clover.com/v2/merchant/" # or https://sandbox.dev.clover.com/v2/merchant/
+target_env = "https://sandbox.dev.clover.com/v2/merchant/"
 orderID = "8GCADRD79S1DW"
-# orderID = "3DJ9RGWSZ9B3A" # test for EU prod order
-API_TOKEN = "1decda79-717f-8ad5-a3d4-f4f6bb0d7ee0" # for Sandbox Test Merchant
-# API_TOKEN = "cbcce4c4-74ec-767b-c34a-7a3def936c78" # for EU prod account
+API_TOKEN = "1decda79-717f-8ad5-a3d4-f4f6bb0d7ee0"
 amount = 1000
 tipAmount = 0
 taxAmount = 0
@@ -22,9 +21,11 @@ expMonth = 12
 expYear = 2018
 CVV = None
 
-########## END SCRIPT CONFIG SETUP ##########
+###############################################
+########## END SCRIPT CONFIG SETUP ############
+###############################################
 
-# Getting secrets to encrypt cc info
+# GET to /v2/merchant/{mId}/pay/key To get the encryption information you’ll need for the pay endpoint.
 url = target_env + merchantID + '/pay/key'
 headers = {"Authorization": "Bearer " + API_TOKEN}
 response = requests.get(url, headers = headers).json()
